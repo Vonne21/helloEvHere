ad198692624aaa005e6efef0db7fffd6a596d3bd
Ng Ee Vonne <eevonne.ng@growthops.asia>
2025-04-20 05:03:47 +0800

    Fix for refresh 404

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import ContactMe from '../views/ContactMe.vue'
 import MyExperience from '../views/MyExperience.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/AboutMe',
